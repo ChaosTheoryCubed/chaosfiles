52367d167c6af55ec1cb22a6659d7e9ffcec3edd
ChaosTheoryCubed <chaostheorycubed@gmail.com>
2024-03-15 09:45:51 -0400

    ♻️: Change colorscheme to have better integrations

--- dot_config/nvim/lua/user/colorscheme.lua
+++ dot_config/nvim/lua/user/colorscheme.lua
@@ -1,11 +1,46 @@
 local M = {
-  "catppuccin/nvim",
-  lazy = false,
-  priority = 1000
+	"catppuccin/nvim",
+	lazy = false,
+	priority = 1000,
 }
 
 function M.config()
-  vim.cmd.colorscheme "catppuccin"
+	require("catppuccin").setup({
+		styles = {
+			comments = { "italic" },
+			keywords = { "italic" },
+		},
+		integrations = {
+			cmp = true,
+			gitsigns = true,
+			nvimtree = true,
+			treesitter = true,
+			neotest = true,
+			native_lsp = {
+				enabled = true,
+				virtual_text = {
+					errors = { "italic" },
+					hints = { "italic" },
+					warnings = { "italic" },
+					information = { "italic" },
+				},
+				underlines = {
+					errors = { "underline" },
+					hints = { "underline" },
+					warnings = { "underline" },
+					information = { "underline" },
+				},
+				inlay_hints = {
+					background = true,
+				},
+			},
+			telescope = {
+				enabled = true,
+			},
+			which_key = true,
+		},
+	})
+	vim.cmd.colorscheme("catppuccin")
 end
 
 return M
