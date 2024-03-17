52c60d7e1acd3a3fceeed61a258a474c16dcf58f
ChaosTheoryCubed <chaostheorycubed@gmail.com>
2024-03-16 22:10:40 -0400

    ✨: Autoformat on save

--- dot_config/nvim/lua/user/none-ls.lua
+++ dot_config/nvim/lua/user/none-ls.lua
@@ -9,14 +9,27 @@ function M.config()
 	local null_ls = require("null-ls")
 	local formatting = null_ls.builtins.formatting
 	local diagnostics = null_ls.builtins.diagnostics
+	local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
 	null_ls.setup({
 		sources = {
 			formatting.stylua,
 			formatting.prettierd,
 			diagnostics.stylelint,
-      diagnostics.golangci_lint
+			diagnostics.golangci_lint,
 		},
+		on_attach = function(client, bufnr)
+			if client.supports_method("textDocument/formatting") then
+				vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+				vim.api.nvim_create_autocmd("BufWritePre", {
+					group = augroup,
+					buffer = bufnr,
+					callback = function()
+						vim.lsp.buf.format({ async = false })
+					end,
+				})
+			end
+		end,
 	})
 end
 
